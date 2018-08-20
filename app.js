74351c252c263e5f28e364f29fee9d8336d43919
Ramon Victor <ramon.victor.n.com>
2018-08-20 17:25:33 -0300

    files publics (css,js) ok

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.set('views', './web/view') // specify the views directory
 app.set('view engine', 'html') // sets the template engine
 app.engine('html',ejs.renderFile);
 
-
+//permitir acesso ao Js e css
+app.use('/static', express.static('web/'));
+//finish
 
 app.get('/', function(req, res) {
     console.log('/ acessada');
--- web/script/login.js
+++ web/script/login.js
@@ -1,1 +1,9 @@
-console.log('teste foi');
+var btLogin = document.querySelector('#login');
+btLogin.addEventListener("click", login);
+
+
+function login(){
+    console.log('login clicado');
+}
+
+console.log('login clicado2');
