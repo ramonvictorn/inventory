e15dc90b56c36736af503c8dc53ec9703a81e49e
Ramon Victor <ramonvictorn@gmail.com>
2018-08-21 17:30:31 -0300

    same render

--- web/script/cadastro.js
+++ web/script/cadastro.js
@@ -1,5 +1,10 @@
 var selecionado;
 
+function start(){ 
+    //$('.padraoPatrimonios').hide();
+}
+start();
+
 $('.tipos').change(function() {
     selecionado = $(this).val();
     console.log(selecionado);
@@ -9,10 +14,16 @@ $('.tipos').change(function() {
 function selecao(){
     if(selecionado == 'pc'){
         console.log('form pc render');
-        renderPc();
+        //renderPc();
+        renderAllPatrimonios()
     }
 }
 
+function renderAllPatrimonios(){
+    $('.padraoPatrimonios').css("display", "block");
+}
+
 function renderPc(){
-    $('.pc').show();
+    //$('.pc').show();
+    renderAllPatrimonios()
 }
